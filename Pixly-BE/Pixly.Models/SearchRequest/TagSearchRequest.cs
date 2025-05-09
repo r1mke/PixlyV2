b061e0c036acd881014e7368e7cd8a33fd63a089
Kerim Begić <kerim.begic@edu.fit.ba>
2025-05-09 16:38:11 +0200

    adding,view and search tags

---
+++ Pixly-BE/Pixly.Models/SearchRequest/TagSearchRequest.cs
@@ -0,0 +1,9 @@
+﻿using Pixly.Models.Pagination;
+
+namespace Pixly.Models.SearchRequest
+{
+    public class TagSearchRequest : PaginationParams
+    {
+        public string? Name { get; set; }
+    }
+}
