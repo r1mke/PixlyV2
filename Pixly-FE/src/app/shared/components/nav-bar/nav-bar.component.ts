f2a303d9aa60c9b4b3aefd05afebd5762f71348b
Kerim Begić <kerim.begic@edu.fit.ba>
2025-05-14 11:56:17 +0200

    nav-bar

--- Pixly-FE/src/app/shared/components/nav-bar/nav-bar.component.ts
+++ Pixly-FE/src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,50 +1,16 @@
-import { Component, EventEmitter, OnInit, Output, HostListener, OnDestroy } from '@angular/core';
+import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
-import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [CommonModule, RouterLink, FormsModule],
+  imports: [CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css'],
 })
 
-export class NavBarComponent implements OnInit,OnDestroy {
-  windowWidth: number = 0;
-  menuOpen: boolean = false;
-  user: any = null;
-  currentSearch: string = '';
-  private ngOnDestroy$ = new Subject<void>();
-  @Output() hoverStateChange = new EventEmitter<{ key: string; state: boolean }>();
+export class NavBarComponent {
+  menuOpen : boolean = false;
+  isLoggedIn : boolean = true;
 
-  @HostListener('window:resize', ['$event'])
-  onResize(event: any) {
-    this.windowWidth = window.innerWidth;
-  }
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute,
-  ) {}
-
-  ngOnInit(): void {
-    this.windowWidth = window.innerWidth;
-  }
-
-  ngOnDestroy(): void {
-    this.ngOnDestroy$.next();
-    this.ngOnDestroy$.complete();
-  }
-
-  toggleMenu() {
-    this.menuOpen = !this.menuOpen;
-  }
-
-  goToHome() {
-    this.router.navigate(['/']);
-  }
 }
