edacd153cd00a13424a7c7b4ab0b8c5d5b42af69
Elmir Mujkic <elmir.mujkic.online@gmail.com>
2025-05-19 11:51:15 +0200

    201 - add interceptors

---
+++ Pixly-FE/src/app/shared/components/submit-button/submit-button.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input} from '@angular/core';
+import {NgClass} from '@angular/common';
+
+@Component({
+  selector: 'app-submit-button',
+  standalone: true,
+  imports: [
+    NgClass
+  ],
+  templateUrl: './submit-button.component.html',
+  styleUrl: './submit-button.component.css'
+})
+export class SubmitButtonComponent {
+  @Input() text: string = 'Submit';
+  @Input() isLoading: boolean = false;
+  @Input() isDisabled: boolean = false;
+  @Input() loadingText: string = 'Loading...';
+  @Input() fullWidth: boolean = true;
+  @Input() buttonType: 'submit' | 'button' = 'submit';
+}
